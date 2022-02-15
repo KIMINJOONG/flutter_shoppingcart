761184c3cba60c9fd00b94a9e321922201422b1b
KIMINJOONG <ijboym216@gmail.com>
2022-02-15 23:54:54 +0900

    setState

--- lib/components/shppingcart_header.dart
+++ lib/components/shppingcart_header.dart
@@ -66,7 +66,11 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
     ),
     child: IconButton(
       icon: Icon(mIcon),
-      onPressed: () {},
+      onPressed: () {
+        setState(() {
+          selectedId = id;
+        });
+      },
     ),
   );
   }
