e591032c6253828892d750ad719e61f09912cb3b
KIMINJOONG <ijboym216@gmail.com>
2022-02-15 23:59:25 +0900

    shopping cart detail init

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -4,6 +4,37 @@ class ShoppingCartDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40)
+      ),
+      child: Column(
+        children:[
+          _buildDetailNameAndPrice(),
+          _buildDetailRatingAndReviewCount(),
+          _buildDetailColorOptions(),
+          _buildDetailButton()
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildDetailNameAndPrice() {
+    return SizedBox();
+  }
+  Widget _buildDetailRatingAndReviewCount() {
+    return SizedBox();
+  }
+  Widget _buildDetailColorOptions() {
+    return SizedBox();
+  }
+  Widget _buildDetailIcon() {
+    return SizedBox();
+  }
+
+  Widget _buildDetailButton() {
     return SizedBox();
   }
 }
