918bd8d762527b9fcf6c8512bd08776ae6fff87a
KIMINJOONG <ijboym216@gmail.com>
2022-02-25 22:18:42 +0900

    shopping cart detail part two stack widget

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
 
@@ -43,27 +44,79 @@ class ShoppingCartDetail extends StatelessWidget {
     );
   }
   Widget _buildDetailRatingAndReviewCount() {
-    return Row(
-      children: [
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Spacer(),
-        Text("review"),
-        Text(
-          "(26)",
-          style: TextStyle(color: Colors.blue),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Spacer(),
+          Text("review"),
+          Text(
+            "(26)",
+            style: TextStyle(color: Colors.blue),
+          ),
+        ],
+      ),
     );
   }
   Widget _buildDetailColorOptions() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color option"),
+          SizedBox(height: 10,),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          ),
+        ],
+      ),
+    );
   }
-  Widget _buildDetailIcon() {
-    return SizedBox();
+
+  // Stack 위젯
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle
+            ),
+          ),
+          Positioned(
+            // 가운데 규칙 : (외부 크기 - 내부 크기) / 2
+            left: 5,
+            top: 5,
+            child: ClipOval(
+              // ClipOval는 위젯이 모양이 잡을 수 있는 속성이 없을때 사용한다.
+              child: Container(
+                color: mColor,
+                width: 40,
+                height: 40,
+
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailButton() {
